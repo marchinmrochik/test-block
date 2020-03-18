f28e3f2883c6e3a9d5757a1d6f6d799f78563857
sveneven <kiselroman280@gmail.com>
2020-03-18 15:27:49 +0200

    added tabs

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
 import $ from 'jquery';
 window.$ = window.jQuery = $;
-import 'slick-carousel'
+import 'slick-carousel';
+
+$( document ).ready(function() {
+    const tabLink = $('.tab__link'), tabContent = $('.tab__content');
+
+    tabLink.on('click', function (e) {
+        e.preventDefault();
+        const _this = $(this);
+        const idContent = _this.attr('href');
+        if(!_this.hasClass('.tab__link_active')) {
+            tabLink.removeClass('tab__link_active');
+            _this.addClass('tab__link_active');
+            tabContent.hide();
+            $(idContent).show();
+        }
+    });
+
+    $('.slider.slider_room').slick({
+
+    })
+});
